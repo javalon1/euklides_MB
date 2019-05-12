3c6639e47b95afebdae941e2d07f384a02b5d291
bajserowiczMikolaj <mbajserowicz@gmail.com>
2019-05-12 12:53:06 +0100

    002

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,5 +1,7 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("podaj A");
+        Scanner scanner =new Scanner(System.in);
+        int A = scanner.nextInt();
+
+        System.out.println("podaj B");
+        Scanner scanner1 =new Scanner(System.in);
+        int B = scanner1.nextInt();
+
+        while (A!=B)
+        {
+           if(A>B){
+               A=A-B;
+           }else {
+               B=B-A;
+           }
+            System.out.println("NWD: "+ A);
+        }
+
+
     }
+
+
 }
+
